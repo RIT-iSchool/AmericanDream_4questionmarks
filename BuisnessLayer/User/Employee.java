3d3490672234336133d9476fe9724e4adac511fe
Steven Chen <131817317+sc66711@users.noreply.github.com>
2023-10-11 18:01:51 -0400

    Add files via upload

--- BuisnessLayer/User/Employee.java
+++ BuisnessLayer/User/Employee.java
@@ -1,87 +1,17 @@
 package BuisnessLayer.User;
 
 public class Employee extends User {
-    
-    private String firstname;
-    private String lastname;
-    private String email;
-    private Society society;
-    private Integer id;
-
     /**
-     * Instantiates an Employee type user
+     * Instantiates a Employee type user by inheriting the user class attributes
      * @param firstname first name of user
      * @param lastname last name of user
      * @param password chosen password
      * @param society society the user belongs to
      * @param email the email of the user
+     * @param id the id of the user
      */
-    public Employee(
-        String firstname,
-        String lastname,
-        String password, 
-        Society society,
-        String email
-        ) {
-        this.society = society;
-        this.email = email;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.id = -1;
+    public Employee(String firstname, String lastname, String password, Society society, String email, Integer id) {
+        super(firstname, lastname, password, society, email, id);
     }
-
-    // getters and setters
-    /**
-     * Get a user's society
-     * @return Society 
-     */
-    public Society getSociety() { return society; }
-    /**
-     * get a user's email
-     * @return String email
-     */
-    public String getEmail() { return email; }
-    /**
-     * Get a user's first name
-     * @return String firstname
-     */
-    public String getFirstName() { return firstname; }
-    /**
-     * Get a user's last name
-     * @return String last name
-     */
-    public String getLastName() { return lastname; }
-    /**
-     * Gets a user's id, will return -1 if it hasn't been set
-     * @return Integer of users id
-     */
-    public Integer getId() { return id; }
-
-    /**
-     * set a user's society
-     * @param society 
-     */
-    public void setSociety( Society society) { }
-    /**
-     * set a user's email
-     * @param email email
-     */
-    public void setEmail( String email ) { }
-    /**
-     * Set a user's first name
-     * @param firstname firstname
-     */
-    public void setFirstName( String firstname ) { }
-    /**
-     * Set a user's last name
-     * @param lastname last name
-     */
-    public void setLastName( String lastname ) { }
-    /**
-     * Sets a user's id
-     * @param id sets the new user id
-     */
-    public void setId( Integer id) { }
-
 
 }
--- BuisnessLayer/User/Member.java
+++ BuisnessLayer/User/Member.java
@@ -6,86 +6,18 @@ import BuisnessLayer.BallotObjects.Ballot;
 
 public class Member extends User {
 
-    private String firstname;
-    private String lastname;
-    private String email;
-    private Society society;
-    private Integer id;
-
     /**
-     * Instantiates a Member type user
+     * Instantiates a Member type user by inheriting the user class attributes
      * @param firstname first name of user
      * @param lastname last name of user
      * @param password chosen password
      * @param society society the user belongs to
      * @param email the email of the user
+     * @param id the id of the user
      */
-    public Member(
-        String firstname,
-        String lastname,
-        String password, 
-        Society society,
-        String email
-        ) {
-        this.society = society;
-        this.email = email;
-        this.firstname = firstname;
-        this.lastname = lastname;
+    public Member(String firstname, String lastname, String password, Society society, String email, Integer id) {
+        super(firstname, lastname, password, email, society, id);
     }
-
-    // getters and setters
-    /**
-     * Get a user's society
-     * @return Society 
-     */
-    public Society getSociety() { return society; }
-    /**
-     * get a user's email
-     * @return String email
-     */
-    public String getEmail() { return email; }
-    /**
-     * Get a user's first name
-     * @return String firstname
-     */
-    public String getFirstName() { return firstname; }
-    /**
-     * Get a user's last name
-     * @return String last name
-     */
-    public String getLastName() { return lastname; }
-    /**
-     * Gets a user's id, will return -1 if it hasn't been set
-     * @return Integer of users id
-     */
-    public Integer getId() { return id; }
-
-    /**
-     * set a user's society
-     * @param society 
-     */
-    public void setSociety( Society society) { }
-    /**
-     * set a user's email
-     * @param email email
-     */
-    public void setEmail( String email ) { }
-    /**
-     * Set a user's first name
-     * @param firstname firstname
-     */
-    public void setFirstName( String firstname ) { }
-    /**
-     * Set a user's last name
-     * @param lastname last name
-     */
-    public void setLastName( String lastname ) { }
-    /**
-     * Sets a user's id
-     * @param id sets the new user id
-     */
-    public void setId( Integer id) { }
-
     /**
      * Gets a list of the electoins the user can vote in
      * @return List of elections a user can vote in, returns an empty list if they have none
