cbbae8af5378a2146c4810c4a0126a7b568c35d9
maija-philip <maija.philip@gmail.com>
2023-10-23 14:32:47 -0400

    added react router navigation

--- PresentationLayer/american-dream/src/pages/App.js
+++ PresentationLayer/american-dream/src/pages/App.js
@@ -1,6 +1,6 @@
-import './assets/css/setup.css';
-import './assets/css/variables.css';
-import './assets/css/styles.css';
+import '../assets/css/setup.css';
+import '../assets/css/variables.css';
+import '../assets/css/styles.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -9,15 +9,19 @@ import '@fontsource/roboto/700.css';
 
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { colors } from './utils/colors.js';
+import { colors } from '../utils/colors.js';
+import { Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      <Button variant="contained">Primary</Button>
       <Typography variant="h1" color={colors['on-surface']}>
-        Heading
+        App
       </Typography>
+      
+      <Link to={'idk'}><Button variant="contained">Go to idk</Button></Link>
+
     </div>
   );
 }
