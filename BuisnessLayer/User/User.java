4befa7698c3075e912828bc8eeb56093cd4b8456
Steven Chen <131817317+sc66711@users.noreply.github.com>
2023-11-29 17:44:37 -0500

    Update User.java for login

--- BuisnessLayer/User/User.java
+++ BuisnessLayer/User/User.java
@@ -1,48 +1,39 @@
 package BuisnessLayer.User;
 
 import java.util.ArrayList;
+import javax.persistence.*;
 
 import BuisnessLayer.BallotObjects.*;
 
-/**
- * Represents a generic User
- * 
- * @author Maija Philip, Steven Chen
- * @version 1.1
- * @since 2023-10-11
- */
-public abstract class User {
-    protected String firstname;
-    protected String lastname;
-    protected String email;
-    protected String password;
-    protected Society society;
-    protected Integer id;
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
+
+    private String firstname;
+    private String lastname;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "SocietyID", nullable = false)
+    private Society society;
     
     /**
-     * Instantiates an user
-     * @param firstname first name of user
-     * @param lastname last name of user
-     * @param password chosen password
-     * @param society society the user belongs to
-     * @param email the email of the user
-     * @param id the id of the user
+     * Instantiates a user
      */
-
-    public User(String firstname, String lastname, String password, Society society, String email, Integer id) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.password = password;
-        this.email = email;
-        this.society = society;
-        this.id = id;
-    }
+    public User() {    }
     
     /**
      * Validates an attempt to login, seeing if the user exists in the database
      * @param username attempted username
      * @param password attempted password
-     * @return Boolean Wether the login credentials matched an existing user or not
+     * @return Boolean Whether the login credentials matched an existing user or not
      */
     public Boolean login(String username, String password) {
         return false;
@@ -50,63 +41,108 @@ public abstract class User {
 
     /**
      * Logout a user
-     * @return Boolean Was the logout successfull?
+     * @return Boolean Was the logout successful?
      */
     public Boolean logout() {
         return false;
     }
     
-    // getters and setters
-    /**
-     * Get a user's society
-     * @return Society 
-     */
-    public Society getSociety() { return society; }
-    /**
-     * get a user's email
-     * @return String email
-     */
-    public String getEmail() { return email; }
-    /**
-     * Get a user's first name
-     * @return String firstname
-     */
-    public String getFirstName() { return firstname; }
-    /**
-     * Get a user's last name
-     * @return String last name
-     */
-    public String getLastName() { return lastname; }
-    /**
-     * Gets a user's id, will return -1 if it hasn't been set
-     * @return Integer of users id
-     */
-    public Integer getId() { return id; }
+    // Getters and Setters
 
     /**
-     * set a user's society
-     * @param society 
+     * Gets the user's ID.
+     * @return int representing the user's ID.
      */
-    public void setSociety( Society society) { }
-    /**
-     * set a user's email
-     * @param email email
-     */
-    public void setEmail( String email ) { }
-    /**
-     * Set a user's first name
-     * @param firstname firstname
-     */
-    public void setFirstName( String firstname ) { }
-    /**
-     * Set a user's last name
-     * @param lastname last name
-     */
-    public void setLastName( String lastname ) { }
-    /**
-     * Sets a user's id
-     * @param id sets the new user id
-     */
-    public void setId( Integer id) { }
+    public int getUserID() {
+        return userID;
+    }
 
+    /**
+     * Sets the user's ID.
+     * @param userID The new user ID.
+     */
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * Gets the user's first name.
+     * @return A String representing the user's first name.
+     */
+    public String getFirstName() { 
+        return firstname; 
+    }
+
+    /**
+     * Sets the user's first name.
+     * @param firstname The new first name of the user.
+     */
+    public void setFirstName(String firstname) {
+        this.firstname = firstname;
+    }
+
+    /**
+     * Gets the user's last name.
+     * @return A String representing the user's last name.
+     */
+    public String getLastName() { 
+        return lastname; 
+    }
+
+    /**
+     * Sets the user's last name.
+     * @param lastname The new last name of the user.
+     */
+    public void setLastName(String lastname) {
+        this.lastname = lastname;
+    }
+
+    /**
+     * Gets the user's email address.
+     * @return A String representing the user's email.
+     */
+    public String getEmail() { 
+        return email; 
+    }
+
+    /**
+     * Sets the user's email address.
+     * @param email The new email address of the user.
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Gets the user's password.
+     * @return A String representing the user's password.
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets the user's password.
+     * @param password The new password of the user.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Gets the society to which the user belongs.
+     * @return The Society object representing the society.
+     */
+    public Society getSociety() { 
+        return society; 
+    }
+
+    /**
+     * Sets the society to which the user belongs.
+     * @param society The new Society object for the user.
+     */
+    public void setSociety(Society society) {
+        this.society = society;
+    }
 }
+
