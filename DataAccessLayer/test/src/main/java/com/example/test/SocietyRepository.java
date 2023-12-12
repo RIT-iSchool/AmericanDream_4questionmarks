6d20eaa3c2256b13e4b95770eba235460e6375d0
sc6671 <sc@6671@rit.edu>
2023-12-12 03:29:14 -0500

    deletes files I moved to other respective folders

--- DataAccessLayer/test/src/main/java/com/example/test/SocietyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.test;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface SocietyRepository extends CrudRepository<Society, Integer> {
-    // Additional query methods if needed
-}
